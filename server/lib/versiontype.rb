6a95f25de61a001845f253013876582077473b57
Jason Heiss <jheiss@aput.net>
2009-04-02 00:20:31 +0000

    Import improved Version class from tpkg.  Many bug fixes.  tpkg has unit tests for the class, any changes should be made and tested there and imported to etch.

--- server/lib/versiontype.rb
+++ server/lib/versiontype.rb
@@ -14,6 +14,25 @@ class Version
   def <=>(other)
     ourfields = @version.split('.')
     otherfields = other.to_s.split('.')
+    # Convert anything like '.5' to '0.5'
+    # '.5'.split('.') returns ['', '5']
+    [ourfields, otherfields].each do |fields|
+      if fields[0] == ''
+        fields[0] = '0'
+      end
+    end
+    # Pad with zeros so that '1' == '1.0', etc.
+    if ourfields.length != otherfields.length
+      larger = [ourfields.length, otherfields.length].max
+      # For the longer number this depends on something like (3...1).each
+      # doing nothing.  That currently works, but is not documented behavior.
+      (ourfields.length...larger).each { ourfields << '0' }
+      (otherfields.length...larger).each { otherfields << '0' }
+    end
+    # Convert fields of all digits from string to number to get a numeric
+    # rather than string comparison.  This ensures that 5.9 < 5.10
+    ourfields.map! { |field| if field =~ /^\d+$/ then field.to_i else field end }
+    otherfields.map! { |field| if field =~ /^\d+$/ then field.to_i else field end }
     # Array conveniently implements <=>
     ourfields <=> otherfields
   end
