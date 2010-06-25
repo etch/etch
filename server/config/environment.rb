685ec48d257b6f7e57e51fe9d2d3a233b418b574
Jason Heiss <jheiss@aput.net>
2010-06-25 17:03:50 +0000

    Update from rails 2.3.4 to rails 2.3.8.
    Update gem name mislav-will_paginate to will_paginate.
    
    Change :session_key to :key per deprecation warning with newer rails
    version.

--- server/config/environment.rb
+++ server/config/environment.rb
@@ -5,7 +5,7 @@
 # ENV['RAILS_ENV'] ||= 'production'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.4' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -25,7 +25,7 @@ Rails::Initializer.run do |config|
   # config.gem "bj"
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "aws-s3", :lib => "aws/s3"
-  config.gem 'mislav-will_paginate', :version => '~> 2.3.8', :lib => 'will_paginate', :source => 'http://gems.github.com'
+  config.gem 'will_paginate', :version => '~> 2.3.8'
 
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
@@ -49,8 +49,8 @@ Rails::Initializer.run do |config|
   # Make sure the secret is at least 30 characters and all random, 
   # no regular words or you'll be exposed to dictionary attacks.
   config.action_controller.session = {
-    :session_key => '_trunk_session',
-    :secret      => '9b1c27a35892d8fbbb50ed309d09c02962f56b739f3ce521c99df3c4f8a2f611bc21ab9fdce060de62b091b06b9e879a7b10a82ab908c286463659f73cace48f'
+    :key    => '_trunk_session',
+    :secret => '9b1c27a35892d8fbbb50ed309d09c02962f56b739f3ce521c99df3c4f8a2f611bc21ab9fdce060de62b091b06b9e879a7b10a82ab908c286463659f73cace48f'
   }
 
   # Use the database for sessions instead of the cookie-based default,
