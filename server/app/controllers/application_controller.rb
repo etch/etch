0ec6d969bdefafbfceca01dc6406e23e6693e116
Jason Heiss <jheiss@aput.net>
2011-05-04 22:07:14 +0000

    Rename etchserver.rb to etch/server.rb to match class structure and keep things cleaner.

--- server/app/controllers/application_controller.rb
+++ server/app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
-require 'etchserver'
+require 'etch/server'
 
 # Filters added to this controller apply to all controllers in the application.
 # Likewise, all the methods added will be available for all controllers.
