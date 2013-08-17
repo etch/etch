34dc6c68dac5fd16d23b1c23e55947564ea065af
Jason Heiss <jheiss@aput.net>
2013-08-17 12:21:49 -0400

    Use a unique pid file for each test server instance
    Rails now complains if you ask it to start a new server instance when
    there's already one running using the default pid file.

--- test/etchtest.rb
+++ test/etchtest.rb
@@ -129,10 +129,11 @@ module EtchTests
         raise "Etch server failed to start"
       end
     else
+      serverargs = "-p #{port} --pid #{SERVERDIR}/tmp/pids/#{port}.pid"
       if `cd #{SERVERDIR} && #{RUBY} \`which bundle\` list`.include?('unicorn')
-        exec("cd #{SERVERDIR} && #{RUBY} `which bundle` exec unicorn -p #{port}")
+        exec("cd #{SERVERDIR} && #{RUBY} `which bundle` exec unicorn #{serverargs}")
       else
-        exec("cd #{SERVERDIR} && #{RUBY} `which bundle` exec rails server -p #{port}")
+        exec("cd #{SERVERDIR} && #{RUBY} `which bundle` exec rails server #{serverargs}")
       end
     end
     {:port => port, :pid => pid, :repo => serverbase}
