fc5bacf3ff62b3207a12b4411d849db5a17334d0
Jason Heiss <jheiss@aput.net>
2014-02-11 20:16:42 -0500

    Bump rails to 4.0.2

--- server/db/schema.rb
+++ server/db/schema.rb
@@ -9,66 +9,66 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20081003164106) do
+ActiveRecord::Schema.define(version: 20081003164106) do
 
-  create_table "clients", :force => true do |t|
-    t.string   "name",       :null => false
+  create_table "clients", force: true do |t|
+    t.string   "name",       null: false
     t.integer  "status"
     t.text     "message"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  add_index "clients", ["name"], :name => "index_clients_on_name", :unique => true
-  add_index "clients", ["status"], :name => "index_clients_on_status"
-  add_index "clients", ["updated_at"], :name => "index_clients_on_updated_at"
+  add_index "clients", ["name"], name: "index_clients_on_name", unique: true
+  add_index "clients", ["status"], name: "index_clients_on_status"
+  add_index "clients", ["updated_at"], name: "index_clients_on_updated_at"
 
-  create_table "etch_configs", :force => true do |t|
-    t.integer  "client_id",  :null => false
-    t.string   "file",       :null => false
-    t.text     "config",     :null => false
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+  create_table "etch_configs", force: true do |t|
+    t.integer  "client_id",  null: false
+    t.string   "file",       null: false
+    t.text     "config",     null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  add_index "etch_configs", ["client_id"], :name => "index_etch_configs_on_client_id"
-  add_index "etch_configs", ["file"], :name => "index_etch_configs_on_file"
+  add_index "etch_configs", ["client_id"], name: "index_etch_configs_on_client_id"
+  add_index "etch_configs", ["file"], name: "index_etch_configs_on_file"
 
-  create_table "facts", :force => true do |t|
-    t.integer  "client_id",  :null => false
-    t.string   "key",        :null => false
-    t.text     "value",      :null => false
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+  create_table "facts", force: true do |t|
+    t.integer  "client_id",  null: false
+    t.string   "key",        null: false
+    t.text     "value",      null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  add_index "facts", ["client_id"], :name => "index_facts_on_client_id"
-  add_index "facts", ["key"], :name => "index_facts_on_key"
+  add_index "facts", ["client_id"], name: "index_facts_on_client_id"
+  add_index "facts", ["key"], name: "index_facts_on_key"
 
-  create_table "originals", :force => true do |t|
-    t.integer  "client_id",  :null => false
-    t.string   "file",       :null => false
-    t.string   "sum",        :null => false
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+  create_table "originals", force: true do |t|
+    t.integer  "client_id",  null: false
+    t.string   "file",       null: false
+    t.string   "sum",        null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  add_index "originals", ["client_id"], :name => "index_originals_on_client_id"
-  add_index "originals", ["file"], :name => "index_originals_on_file"
+  add_index "originals", ["client_id"], name: "index_originals_on_client_id"
+  add_index "originals", ["file"], name: "index_originals_on_file"
 
-  create_table "results", :force => true do |t|
-    t.integer  "client_id",  :null => false
-    t.string   "file",       :null => false
-    t.boolean  "success",    :null => false
-    t.text     "message",    :null => false
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+  create_table "results", force: true do |t|
+    t.integer  "client_id",  null: false
+    t.string   "file",       null: false
+    t.boolean  "success",    null: false
+    t.text     "message",    null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  add_index "results", ["client_id"], :name => "index_results_on_client_id"
-  add_index "results", ["created_at"], :name => "index_results_on_created_at"
-  add_index "results", ["file"], :name => "index_results_on_file"
+  add_index "results", ["client_id"], name: "index_results_on_client_id"
+  add_index "results", ["created_at"], name: "index_results_on_created_at"
+  add_index "results", ["file"], name: "index_results_on_file"
 
 end
